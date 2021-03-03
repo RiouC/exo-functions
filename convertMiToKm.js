85fee5afb9832ca2270bd61b04f1d51852450b4f
RiouC <riou.christophe@protonmail.com>
2021-03-03 15:14:33 +0100

    convertmitokm: add convertmitokm.js

---
+++ convertMiToKm.js
@@ -0,0 +1,7 @@
+const convertMiToKm = (distanceMiles) => {
+    return parseFloat(distanceMiles) * 1.60934;
+}
+
+// console.log(convertMiToKm(1));
+// console.log(convertMiToKm(2));
+// console.log(convertMiToKm(5.6));
